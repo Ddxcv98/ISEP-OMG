d8d227bb56d6107c8f17271d3a88c3fcbac0a7a9
David <Ddxcv98@gmail.com>
2020-06-21 14:44:43 +0100

    OMG

---
+++ src/script.js
@@ -0,0 +1,60 @@
+const { remote } = require('electron');
+
+const fs = require('fs');
+
+const classes = document.getElementById('classes');
+const search = document.getElementById('search');
+const table = document.getElementById('table');
+let map;
+
+classes.addEventListener('input', () => {
+    const text = classes.value.toLowerCase();
+    map = new Map();
+    table.innerHTML = '';
+    search.value = '';
+    
+    fs.readFile(`${remote.app.getAppPath()}/raw/perguntas_${text}.txt`, 'utf8', (err, dQuestions) => {
+        if (err) throw err;
+
+        fs.readFile(`${remote.app.getAppPath()}/raw/respostas_${text}.txt`, 'utf8', (err, dAnswers) => {
+            if (err) throw err;
+            const questions = dQuestions.split(/(\n|\r|\r\n)/g);
+            const answers = dAnswers.split(/(\n|\r|\r\n)/g);
+            
+            let j = 0;
+            
+            for (const question of questions) {
+                let answer = answers[j].trim();
+
+                while (j < answers.length && answer.length === 0) {
+                    answer = answers[++j].trim();
+                }
+
+                map.set(question.trim(), answer);
+
+                do {
+                    j++
+                } while (j < answers.length && answers[j].trim().length !== 0);
+            }
+        });
+    });
+});
+
+search.addEventListener('input', () => {
+    const text = search.value;
+    table.innerHTML = '';
+
+    if (text.length > 0) {
+        map.forEach((value, key) => {
+            if (key.toLowerCase().includes(text.toLowerCase())) {
+                const tr = document.createElement('tr');
+                const question = document.createElement('td');
+                const answer = document.createElement('td');
+                question.innerText = key;
+                answer.innerText = value;
+                tr.append(question, answer);
+                table.append(tr);
+            }
+        });
+    }
+});
